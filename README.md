653a6bc11d625bcf0e14638f0b79323696dd7b5d
umami <umami@localhost.localdomain>
2015-06-08 21:20:58 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# gulp-jade-template-concat
+Compiles Jade templates single file containing template functions.  For projects small enough that they don't require AMD or CommonJS loaders.
+
+##Install
+```sh
+$ npm install --save-dev gulp-jade-template-concat
+```
+
+##Usage
+###Gulpfile
+```javascript
+
+var jade = require('gulp-jade');
+var jadeConcat = require('gulp-jade-template-concat');
+
+
+gulp.task("client-templates", function(){
+    var dest = 'build/templates/';
+    gulp.src('src/jade/templates/**/*.jade')
+        .pipe(jade({
+            client: true
+        })
+        .pipe(jadeConcat('mytemplates.js', {templateVariable:"templates"}))
+        .pipe(gulp.dest('build/templates/'))
+});
+
+```
+
+This compiles all of your client side jade templates into a file called `mytemplates.js`.  The `templateVariable` option is optional and will default to `templates` if it is not set.
+
+
+###HTML/Jade file
+Link the concatenated file with a script tag
+```jade
+script(src="templates/mytemplates.js")
+```
+
+Access the generated templates using dot or bracket access notation.
+```javascript
+  templates['template1'];
+  templates.template2;
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+var gutil = require('gulp-util');
+var PluginError = gUtil.PluginError;
+var through = require('through');
+
+
+function pluginError (message) {
+  return new PluginError('gulp-jade-template-concat', message)
+}
+
+
+module.exports = function jadeConcat(fileName, _opts) {
+  if (!fileName) throw pluginError('Missing fileName')
+
+  console.log(templateVariable);
+  var defaults = {templateVariable: "templates"}
+  var concatString = "";
+
+
+  function write (file) {
+    if (file.isNull()) return
+    if (file.isStream()) return this.emit('error', pluginError('Streaming not supported'))
+
+    //isolate filename from full path
+    var filename = file.path.replace(file.base, "").replace(".js", "");
+
+    // replace template name with filename
+    var contents = file.contents.toString().replace('function template', '"' + filename + '": function')
+
+    concatString += contents + ",\n";;
+  }
+
+  function end () {
+    //wrap concatenated string in template object
+    var templateString = "var " + opts.templateVariable + " = {\n" + concatString + "}";
+
+    this.queue(new gutil.File({
+      path: fileName,
+      contents: new Buffer(templateString)
+    }))
+
+    this.queue(null)
+  }
+
+  return through(write, end)
+}
